e91947119d605ad31f3386d418a80a48f3ce971e
Veedy <38901140+veedy-dev@users.noreply.github.com>
2022-05-15 14:02:58 +0700

    Initial commit

---
+++ Il2CppDumper/Il2Cpp/MetadataDecryption.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Il2CppDumper
+{
+    public partial class MetadataDecryption
+    {
+        private static byte XorCombine(byte[] input, int offset, int size = 16)
+        {
+            byte ret = 0;
+            for (int i = offset; i < offset + size; i++)
+                ret ^= input[i];
+            return ret;
+        }
+
+        private static void CompressKey(byte[] data, byte[] key)
+        {
+            for (int i = 0; i < 0xB0; i++)
+                key[i] = XorCombine(data, 16 * i);
+        }
+
+        // this is mostly the same algorithm as in blkstuff
+        // except i can't just hardcode some of the stuff since the key changes between versions this time
+        // the modifications made come from a tool to decrypt metadata that already exists,
+        // since i'm too much of a dumbass to figure out how the key data gets derived myself
+        private static void NotBlkKeyScramble(byte[] key, byte[] data, int offset)
+        {
+            for (int i = 0; i < 16; i++)
+                data[i + offset] ^= key[i];
+
+            byte[] indexScramble = new byte[]
+            {
+                0,  13, 10, 7,
+                4,  1,  14, 11,
+                8,  5,  2,  15,
+                12, 9,  6,  3
+            };
+            uint[] scratch = new uint[4];
+            byte[] scratchByte = new byte[16]; // c# so no pointer casting
+            for (int i = 1; i < 10; i++)
+            {
+                // avoid reallocating
+                for (int j = 0; j < 4; j++)
+                    scratch[j] = 0;
+                for (int j = 0; j < 4; j++)
+                {
+                    scratch[j] ^= BlkStuff1p2[data[indexScramble[4 * j + 0] + offset]];
+                    scratch[j] ^= BlkStuff1p3[data[indexScramble[4 * j + 1] + offset]];
+                    scratch[j] ^= BlkStuff1p4[data[indexScramble[4 * j + 2] + offset]];
+                    scratch[j] ^= BlkStuff1p5[data[indexScramble[4 * j + 3] + offset]];
+                }
+                Buffer.BlockCopy(scratch, 0, scratchByte, 0, scratchByte.Length);
+                for (int j = 0; j < 16; j++)
+                    data[j + offset] = (byte)(scratchByte[j] ^ key[16 * i + j]);
+            }
+
+            for (int i = 0; i < 16; i++)
+            {
+                byte t = data[indexScramble[i] + offset];
+                scratchByte[i] = (byte)(BlkStuff1p6[t] ^ ~t);
+            }
+
+            for (int i = 0; i < 16; i++)
+                data[i + offset] = (byte)(scratchByte[i] ^ key[160 + i]);
+        }
+
+        // the original metadata decryption tool also decrypted string literals with this step
+        // but since i already have an implementation of that stuff used elsewhere (and because c# won't let me cast pointers),
+        // this will just return the required data
+        public struct StringDecryptionData
+        {
+            public uint stringCountXor;
+            public uint stringOffsetXor;
+            public uint stringLiteralOffsetXor;
+            public uint stringLiteralDataCountXor;
+            public uint stringLiteralDataOffsetXor;
+            public byte[] stringDecryptionBlob;
+        }
+        private static StringDecryptionData DecryptMetadataStringInfo(byte[] metadata)
+        {
+            var data = new StringDecryptionData();
+
+            var values = new uint[0x12];
+            Buffer.BlockCopy(metadata, 0x60,  values, 0,  16);
+            Buffer.BlockCopy(metadata, 0x140, values, 16, 16);
+            Buffer.BlockCopy(metadata, 0x100, values, 32, 16);
+            Buffer.BlockCopy(metadata, 0xF0,  values, 48, 8);
+            Buffer.BlockCopy(metadata, 0x8,   values, 56, 16);
+
+            ulong seed = ((ulong)values[values[0] & 0xF] << 32) | values[(values[0x11] & 0xF) + 2];
+            var rand = new MT19937_64(seed);
+
+            data.stringCountXor = (uint)rand.Int63();
+            data.stringOffsetXor = (uint)rand.Int63();
+            rand.Int63();
+            data.stringLiteralOffsetXor = (uint)rand.Int63();
+            data.stringLiteralDataCountXor = (uint)rand.Int63();
+            data.stringLiteralDataOffsetXor = (uint)rand.Int63();
+
+            var stringDecryptionBlob = new ulong[0x5000 / 8];
+            for (int i = 0; i < stringDecryptionBlob.Length; i++)
+                stringDecryptionBlob[i] = rand.Int63();
+            data.stringDecryptionBlob = new byte[0x5000];
+            Buffer.BlockCopy(stringDecryptionBlob, 0, data.stringDecryptionBlob, 0, 0x5000);
+
+            return data;
+        }
+
+        private static void DecryptMetadataBlocks(byte[] metadata)
+        {
+            byte[] footer = new byte[0x4000];
+            Buffer.BlockCopy(metadata, metadata.Length - footer.Length, footer, 0, footer.Length);
+
+            if (footer[0xC8] != 0x2E ||
+                footer[0xC9] != 0xFC ||
+                footer[0xCA] != 0xFE ||
+                footer[0xCB] != 0x2C)
+                throw new ArgumentException("*((uint32_t*)&footer[0xC8]) != 0x2CFEFC2E");
+
+            byte[] out1 = new byte[0xB00];
+            byte[] out2 = new byte[0x10];
+            ushort offset = (ushort)((footer[0xD3] << 8) | footer[0xD2]);
+            Buffer.BlockCopy(footer, offset, out2, 0, out2.Length);
+            Buffer.BlockCopy(footer, offset + 0x10, out1, 0, out1.Length);
+
+            for (int i = 0; i < 0x10; i++)
+                out2[i] ^= footer[0x3000 + i];
+            for (int i = 0; i < 0xB00; i++)
+                out1[i] ^= (byte)(footer[0x3010 + i] ^ out2[i % 0x10]);
+
+            byte[] temp = new byte[0x10];
+            byte[] scratch = new byte[0x10];
+
+            byte[] hardKey = new byte[]
+            {
+                0xAD, 0x2F, 0x42, 0x30, 0x67, 0x04, 0xB0, 0x9C, 0x9D, 0x2A, 0xC0, 0xBA, 0x0E, 0xBF, 0xA5, 0x68
+            };
+            byte[] key = new byte[0xB0];
+            CompressKey(out1, key);
+
+            var entrySize = (metadata.Length / 0x100) / 0x40 * 0x40;
+            for (int k = 0; k < 0x100; k++)
+            {
+                var off = k * entrySize;
+
+                for (int i = 0; i < 16; i++)
+                    temp[i] = (byte)(out2[i] ^ hardKey[i]);
+
+                for (int i = 0; i < 0x40; i += 0x10)
+                {
+                    Buffer.BlockCopy(temp, 0, scratch, 0, 0x10);
+                    Buffer.BlockCopy(metadata, off + i, temp, 0, 0x10);
+                    NotBlkKeyScramble(key, metadata, off + i);
+                    for (int j = 0; j < 0x10; j++)
+                        metadata[off + i + j] ^= scratch[j];
+                }
+            }
+        }
+
+        public static StringDecryptionData DecryptMetadata(byte[] metadata)
+        {
+            DecryptMetadataBlocks(metadata);
+            return DecryptMetadataStringInfo(metadata);
+        }
+    }
+}
---
+++ Il2CppDumper/Program.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+using System.Diagnostics;
+#if NETFRAMEWORK
+using System.Windows.Forms;
+#endif
+
+namespace Il2CppDumper
+{
+    class Program
+    {
+        private static Config config;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"config.json"));
+            string il2cppPath = null;
+            string metadataPath = null;
+            string nameTranslationPath = null;
+            string outputDir = null;
+
+            if (args.Length == 1)
+            {
+                if (args[0] == "-h" || args[0] == "--help" || args[0] == "/?" || args[0] == "/h")
+                {
+                    ShowHelp();
+                    return;
+                }
+            }
+            if (args.Length > 3)
+            {
+                ShowHelp();
+                return;
+            }
+            if (args.Length > 1)
+            {
+                foreach (var arg in args)
+                {
+                    if (File.Exists(arg))
+                    {
+                        var file = File.ReadAllBytes(arg);
+                        // TODO: Make args decent lol
+                        if (arg.Contains(".dat"))
+                        {
+                            metadataPath = arg;
+                        }
+                        else
+                        {
+                            il2cppPath = arg;
+                        }
+                    }
+                    else if (Directory.Exists(arg))
+                    {
+                        outputDir = Path.GetFullPath(arg) + Path.DirectorySeparatorChar;
+                    }
+                }
+            }
+            if (outputDir == null)
+            {
+                outputDir = AppDomain.CurrentDomain.BaseDirectory;
+            }
+#if NETFRAMEWORK
+            if (il2cppPath == null)
+            {
+                var ofd = new OpenFileDialog();
+                ofd.Filter = "UserAssembly|UserAssembly.dll";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    il2cppPath = ofd.FileName;
+                    ofd.Filter = "global-metadata|global-metadata.dat";
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        metadataPath = ofd.FileName;
+                        ofd.Title = "Open nameTranslation.txt if you have one, otherwise just hit cancel";
+                        ofd.Filter = "BeeByte Obfuscator mappings|nameTranslation.txt";
+                        if (ofd.ShowDialog() == DialogResult.OK)
+                            nameTranslationPath = ofd.FileName;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+#endif
+            if (il2cppPath == null)
+            {
+                ShowHelp();
+                return;
+            }
+            try
+            {
+                if (Init(il2cppPath, metadataPath, nameTranslationPath, out var metadata, out var il2Cpp))
+                {
+                    Dump(metadata, il2Cpp, outputDir);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            if (config.RequireAnyKey)
+            {
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey(true);
+            }
+        }
+
+        static void ShowHelp()
+        {
+            Console.WriteLine($"usage: {AppDomain.CurrentDomain.FriendlyName} <executable-file> <global-metadata> <output-directory>");
+        }
+
+        private static bool Init(string il2cppPath, string metadataPath, string nameTranslationPath, out Metadata metadata, out Il2Cpp il2Cpp)
+        {
+            Console.WriteLine("Initializing metadata...");
+            var metadataBytes = File.ReadAllBytes(metadataPath);
+
+            var stringDecryptionInfo = MetadataDecryption.DecryptMetadata(metadataBytes);
+
+            metadata = new Metadata(new MemoryStream(metadataBytes), stringDecryptionInfo, nameTranslationPath);
+            Console.WriteLine($"Metadata Version: {metadata.Version}");
+
+            Console.WriteLine("Initializing il2cpp file...");
+            var il2cppBytes = File.ReadAllBytes(il2cppPath);
+            var il2cppMagic = BitConverter.ToUInt32(il2cppBytes, 0);
+            var il2CppMemory = new MemoryStream(il2cppBytes);
+            switch (il2cppMagic)
+            {
+                default:
+                    throw new NotSupportedException("ERROR: il2cpp file not supported.");
+                case 0x6D736100:
+                    var web = new WebAssembly(il2CppMemory);
+                    il2Cpp = web.CreateMemory();
+                    break;
+                case 0x304F534E:
+                    var nso = new NSO(il2CppMemory);
+                    il2Cpp = nso.UnCompress();
+                    break;
+                case 0x905A4D: //PE
+                    il2Cpp = new PE(il2CppMemory);
+                    break;
+                case 0x464c457f: //ELF
+                    if (il2cppBytes[4] == 2) //ELF64
+                    {
+                        il2Cpp = new Elf64(il2CppMemory);
+                    }
+                    else
+                    {
+                        il2Cpp = new Elf(il2CppMemory);
+                    }
+                    break;
+                case 0xCAFEBABE: //FAT Mach-O
+                case 0xBEBAFECA:
+                    var machofat = new MachoFat(new MemoryStream(il2cppBytes));
+                    Console.Write("Select Platform: ");
+                    for (var i = 0; i < machofat.fats.Length; i++)
+                    {
+                        var fat = machofat.fats[i];
+                        Console.Write(fat.magic == 0xFEEDFACF ? $"{i + 1}.64bit " : $"{i + 1}.32bit ");
+                    }
+                    Console.WriteLine();
+                    var key = Console.ReadKey(true);
+                    var index = int.Parse(key.KeyChar.ToString()) - 1;
+                    var magic = machofat.fats[index % 2].magic;
+                    il2cppBytes = machofat.GetMacho(index % 2);
+                    il2CppMemory = new MemoryStream(il2cppBytes);
+                    if (magic == 0xFEEDFACF)
+                        goto case 0xFEEDFACF;
+                    else
+                        goto case 0xFEEDFACE;
+                case 0xFEEDFACF: // 64bit Mach-O
+                    il2Cpp = new Macho64(il2CppMemory);
+                    break;
+                case 0xFEEDFACE: // 32bit Mach-O
+                    il2Cpp = new Macho(il2CppMemory);
+                    break;
+            }
+            var version = config.ForceIl2CppVersion ? config.ForceVersion : metadata.Version;
+            il2Cpp.SetProperties(version, metadata.maxMetadataUsages);
+            Console.WriteLine($"Il2Cpp Version: {il2Cpp.Version}");
+            if (il2Cpp.Version >= 27 && il2Cpp is ElfBase elf && elf.IsDumped)
+            {
+                Console.WriteLine("Input global-metadata.dat dump address:");
+                metadata.Address = Convert.ToUInt64(Console.ReadLine(), 16);
+            }
+
+
+            Console.WriteLine("Searching...");
+            try
+            {
+                //var flag = il2Cpp.PlusSearch(metadata.methodDefs.Count(x => x.methodIndex >= 0), metadata.typeDefs.Length);
+                var flag = false;
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    if (!flag && il2Cpp is PE)
+                    {
+                        Console.WriteLine("Use custom PE loader");
+                        il2Cpp = PELoader.Load(il2cppPath);
+                        il2Cpp.SetProperties(version, metadata.maxMetadataUsages);
+                        //flag = il2Cpp.PlusSearch(metadata.methodDefs.Count(x => x.methodIndex >= 0), metadata.typeDefs.Length);
+                    }
+                }
+                /*if (!flag)
+                {
+                    flag = il2Cpp.Search();
+                }
+                if (!flag)
+                {
+                    flag = il2Cpp.SymbolSearch();
+                }*/
+                if (true)
+                {
+                    /*Console.WriteLine("ERROR: Can't use auto mode to process file, try manual mode.");
+                    Console.Write("Input CodeRegistration: ");
+                    var codeRegistration = Convert.ToUInt64(Console.ReadLine(), 16);
+                    Console.Write("Input MetadataRegistration: ");
+                    var metadataRegistration = Convert.ToUInt64(Console.ReadLine(), 16);*/
+                    ProcessModuleCollection pms = Process.GetCurrentProcess().Modules;
+                    ulong baseaddr = 0;
+                    ProcessModule targetModule = null;
+                    foreach (ProcessModule pm in pms)
+                    {
+                        if (pm.ModuleName == "UserAssembly.dll")
+                        {
+                            baseaddr = (ulong)pm.BaseAddress;
+                            targetModule = pm;
+                            break;
+                        }
+                    }
+                    Console.WriteLine("baseadr: 0x" + baseaddr.ToString("x2"));
+
+                    ulong codeRegistration = 0;
+                    ulong metadataRegistration = 0;
+
+                    // custom search
+                    // searching .text for the following pattern:
+                    // lea r8,  [rip+0x????????]
+                    // lea rdx, [rip+0x????????]
+                    // lea rcx, [rip+0x????????]
+                    // jmp [rip+0x????????]
+                    // or...
+                    // 4c 8d 05 ?? ?? ?? ??
+                    // 48 8d 15 ?? ?? ?? ??
+                    // 48 8d 0d ?? ?? ?? ??
+                    // e9
+                    // 22 bytes long
+
+                    // .text is always the first section
+                    var text_start = ((PE)il2Cpp).Sections[0].VirtualAddress + baseaddr;
+                    var text_end = text_start + ((PE)il2Cpp).Sections[0].VirtualSize;
+
+                    // functions are always aligned to 16 bytes
+                    const int patternLength = 22;
+                    byte[] d = new byte[patternLength];
+                    for (ulong ptr = text_start; ptr < text_end - patternLength; ptr += 0x10)
+                    {
+                        Marshal.Copy((IntPtr)ptr, d, 0, patternLength);
+                        if (
+                            d[ 0] == 0x4C && d[ 1] == 0x8D && d[ 2] == 0x05 &&
+                            d[ 7] == 0x48 && d[ 8] == 0x8D && d[ 9] == 0x15 &&
+                            d[14] == 0x48 && d[15] == 0x8D && d[16] == 0x0D &&
+                            d[21] == 0xE9
+                        )
+                        {
+                            codeRegistration = ptr + 21 + BitConverter.ToUInt32(d, 14 + 3);
+                            metadataRegistration = ptr + 14 + BitConverter.ToUInt32(d, 7 + 3);
+                            Console.WriteLine($"Found the offsets! codeRegistration: 0x{(codeRegistration - baseaddr).ToString("X2")}, metadataRegistration: 0x{(metadataRegistration - baseaddr).ToString("X2")}");
+                            break;
+                        }
+                    }
+
+                    if (codeRegistration == 0 && metadataRegistration == 0)
+                    {
+                        Console.WriteLine("Failed to find CodeRegistration and MetadataRegistration, go yell at Khang");
+                        return false;
+                    }
+
+                    il2Cpp.Init(codeRegistration, metadataRegistration);
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("ERROR: An error occurred while processing.");
+                return false;
+            }
+            return true;
+        }
+
+        private static void Dump(Metadata metadata, Il2Cpp il2Cpp, string outputDir)
+        {
+            Console.WriteLine("Dumping...");
+            var executor = new Il2CppExecutor(metadata, il2Cpp);
+            var decompiler = new Il2CppDecompiler(executor);
+            decompiler.Decompile(config, outputDir);
+            Console.WriteLine("Done!");
+            if (config.GenerateScript)
+            {
+                Console.WriteLine("Generate script...");
+                var scriptGenerator = new ScriptGenerator(executor);
+                scriptGenerator.WriteScript(outputDir);
+                Console.WriteLine("Done!");
+            }
+            if (config.GenerateDummyDll)
+            {
+                Console.WriteLine("Generate dummy dll...");
+                DummyAssemblyExporter.Export(executor, outputDir);
+                Console.WriteLine("Done!");
+            }
+        }
+    }
+}
